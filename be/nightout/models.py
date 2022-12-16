84dc850a48c2880ee93efdea9b725cb3f5829514
TobiToxota <tobias.seiler@posteo.de>
2022-12-16 10:44:33 +0100

    fixed bug in NightOutModel uuid

--- be/nightout/models.py
+++ be/nightout/models.py
@@ -1,4 +1,4 @@
-from click import UUID
+import uuid
 from django.db import models
 from django.conf import settings
 
@@ -14,7 +14,7 @@ class NightOutModel(models.Model):
     creator = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="createdNightOuts")
     title = models.CharField(max_length=40, )
-    uuid = models.UUIDField(primary_key=True, default=UUID.uuid4, editable=False)
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     description = models.CharField(max_length=200, blank=True)
     createdAt = models.DateTimeField(auto_now_add=True)
     updatedAt = models.DateTimeField(auto_now=True)
