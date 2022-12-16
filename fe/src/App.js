ef6c8d6d86fd96eec28afaa090d4334d59c5f8ba
TobiToxota <tobias.seiler@posteo.de>
2022-12-16 16:12:43 +0100

    updadet gitignore

--- fe/src/App.js
+++ fe/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Hallo asdasdasdasdasdasdasd
+          Edit <code>src/App.js</code> and save to reload asdasdasdsa. Hallo
         </p>
         <a
           className="App-link"
