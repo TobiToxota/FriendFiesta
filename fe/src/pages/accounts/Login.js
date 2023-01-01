7d374ceee8c2fbd0447670cfb8402eead7811b7b
TobiToxota <tobias.seiler@posteo.de>
2023-01-01 06:02:15 +0000

    added handling if user is allready logged in to loginPage

--- fe/src/pages/accounts/Login.js
+++ fe/src/pages/accounts/Login.js
@@ -7,62 +7,84 @@ import Notification from "../../components/common/Notification";
 
 function Login() {
   // create the login function
-  let { loginUser, message, setMessage  } = useContext(AuthContext);
+  let { loginUser, message, setMessage, userData } = useContext(AuthContext);
 
   return (
     <section className="hero is-info is-fullheight">
       <div className="hero-body">
         <div className="login-container">
           <p className="title">Login</p>
-          <p className="subtitle">Welcome to ltshangout</p>
-          <form onSubmit={loginUser}>
-            <div className="field">
-              <p className="control has-icons-left has-icons-right">
-                <input
-                  className="input is-rounded"
-                  type="text"
-                  placeholder="Username"
-                  name="username"
-                />
-                <span className="icon is-small is-left">
-                  <i className="fa-solid fa-user-astronaut" />
-                </span>
-                <span className="icon is-small is-right">
-                  <i className="fas fa-check" />
-                </span>
-              </p>
-            </div>
-            <div className="field">
-              <p className="control has-icons-left has-icons-right">
-                <input
-                  className="input is-rounded"
-                  type="password"
-                  placeholder="Password"
-                  name="password"
-                />
-                <span className="icon is-small is-left">
-                  <i className="fas fa-lock" />
-                </span>
-                <span className="icon is-small is-right">
-                  <i className="fas fa-check" />
-                </span>
-              </p>
-            </div>
-            <div className="field">
-              <p className="control">
-                <button
-                  className="button is-success is-rounded"
-                  type="submit"
-                  value="Login">
-                  Login
-                </button>
-              </p>
-            </div>
-          </form>
+          {userData ?
+            <>
+              <p className="subtitle mb-2">Hi {userData.username} you are allready logged in. <br></br> Please logout first or go back to ltshangout.</p>
+              <Link to={'/'}><button
+                className="button is-link is-rounded mr-1"
+                value="Logout">
+                Back to ltshangout
+              </button></Link>
+            </>
+            : <p className="subtitle">Welcome to ltshangout</p>
+          }
+          {!userData ?
+            <form onSubmit={loginUser}>
+              <div className="field">
+                <p className="control has-icons-left has-icons-right">
+                  <input
+                    className="input is-rounded"
+                    type="text"
+                    placeholder="Username"
+                    name="username"
+                  />
+                  <span className="icon is-small is-left">
+                    <i className="fa-solid fa-user-astronaut" />
+                  </span>
+                  <span className="icon is-small is-right">
+                    <i className="fas fa-check" />
+                  </span>
+                </p>
+              </div>
+              <div className="field">
+                <p className="control has-icons-left has-icons-right">
+                  <input
+                    className="input is-rounded"
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                  />
+                  <span className="icon is-small is-left">
+                    <i className="fas fa-lock" />
+                  </span>
+                  <span className="icon is-small is-right">
+                    <i className="fas fa-check" />
+                  </span>
+                </p>
+              </div>
+              <div className="field">
+                <p className="control">
+                  <button
+                    className="button is-success is-rounded"
+                    type="submit"
+                    value="Login">
+                    Login
+                  </button>
+                </p>
+              </div>
+            </form>
+            : <>
+              <button
+                className="button is-success is-rounded"
+                value="Logout">
+                Logout
+              </button>
+            </>}
+          {!userData ? 
+          <>
           <p className="is-family-code mt-3">You dont have an account?</p>
           <Link to="/register">
             <p className="is-family-code is-size-5">Register</p>
           </Link>
+          </>
+          : null }
           {message && (
             <Notification msg={message} onExit={() => setMessage(null)} />
           )}
