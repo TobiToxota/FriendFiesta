694065bbb1bc90fb41eeceb90ef7091d8ee21b49
TobiToxota <tobias.seiler@posteo.de>
2023-01-05 17:15:31 +0000

    added styling for main container for nightouts

--- fe/src/components/features/NightOutTopComponent.js
+++ fe/src/components/features/NightOutTopComponent.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ProgressComponent from "../common/ProgressComponent";
 import DropDownContentComponent from "../common/DropDownContentComponent";
 
-const NightOutTopComponent = ({nightOut, userData}) => {
+const NightOutTopComponent = ({ nightOut, userData }) => {
+  const [addParticipantHandler, setAddParticipant] = useState(false);
 
-    return (
-        <>
+  return (
+    <>
+      <div
+        className="container is-fluid active is-rounded"
+        id="main-container">
+        <div
+          className="notification is-light is-rounded main-nightout-container fade-in"
+          style={{ marginTop: "8vh", borderRadius: 15, minHeight: "150px" }}>
           <h3 className="subtitle is-3 mb-2">Title: {nightOut.title}</h3>
           <ProgressComponent percentage={25}></ProgressComponent>
           <div id="creator" className="mt-2 is-inline-block is-vcentered is-flex">
@@ -46,7 +53,6 @@ const NightOutTopComponent = ({nightOut, userData}) => {
             </button>
             {addParticipantHandler ? (
               <form
-                onSubmit={addParticipant}
                 className="is-inline ml-1 fade-in"
                 id="addParticipantForm">
                 <div className="field is-inline">
@@ -67,21 +73,19 @@ const NightOutTopComponent = ({nightOut, userData}) => {
                     </button>
                   </div>
                   <button
-                    className="button is-danger is-rounded is-small mt-1 ml-1"
-                    onClick={() => {
-                      setAddParticipant(false, setErrormsg(false));
-                    }}>
+                    className="button is-danger is-rounded is-small mt-1 ml-1">
                     cancel
                   </button>
                 </div>
               </form>
             ) : null}
             <div>
-              {errormsg ? <Notificaton msg={errormsg} onExit={onExit} /> : null}
             </div>
           </div>
-        </>
-      );
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default NightOutTopComponent;
