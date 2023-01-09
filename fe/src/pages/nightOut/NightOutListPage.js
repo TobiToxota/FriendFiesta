9861a1683cc9d15ff27af6929124a72e45da7258
TobiToxota <tobias.seiler@posteo.de>
2023-01-09 14:11:23 +0000

    introduced squarepeg font face

--- fe/src/pages/nightOut/NightOutListPage.js
+++ fe/src/pages/nightOut/NightOutListPage.js
@@ -1,8 +1,7 @@
 /** @format */
 
 // package imports
-import React, { useContext, useEffect } from "react";
-import anime from "animejs";
+import React, { useContext } from "react";
 
 // local imports
 import HeaderComponent from "../../components/layout/HeaderComponent";
@@ -10,14 +9,11 @@ import SpinnerComponent from "../../components/common/SpinnerComponent";
 import AuthContext from "../../context/AuthContext";
 import { useGetNightOutList } from "../../hooks/api/nightOutAPI";
 import NightOutListComponent from "../../components/features/NightOutListComponent";
-import { useFading } from "../../hooks/animations/animations";
 
 const NightOutListPage = () => {
   // get userData, token and nightOutlist
-  const { token } = useContext(AuthContext);
+  const { token, userData } = useContext(AuthContext);
   const { nightOutList, loading } = useGetNightOutList(token);
-
-  useFading('#' + 1)
 
   return (
     <>
@@ -30,10 +26,11 @@ const NightOutListPage = () => {
         <>
           <HeaderComponent />
           <div className="container mt-6" id="wholeList">
+          <h1 className="subtitle is-1 has-text-centered squarepeg" style={{color: 'white'}}>Your Nightouts:</h1>
             <div className="p-4 columns is-centered is-flex-wrap-wrap has-text-centered">
               {nightOutList.map((nightOut, index) => (
                 <div id={1} key={index}>
-                <NightOutListComponent nightOut={nightOut} key={index}/>
+                  <NightOutListComponent nightOut={nightOut} key={index} />
                 </div>
               ))}
             </div>
