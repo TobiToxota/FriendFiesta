65efb7068a3f155982cc8fe2aae1572ae194d7aa
TobiToxota <tobias.seiler@posteo.de>
2023-01-13 17:31:40 +0000

    added post view for creating Notifications

--- be/nightout/api/views.py
+++ be/nightout/api/views.py
@@ -424,7 +424,7 @@ class FindFinalSuggestionForFinish(APIView):
 
 
 @permission_classes((IsAuthenticated,))
-class GetNotifications(APIView):
+class NotificationsView(APIView):
     def get(self, request, format=None):
 
         #get all Notifications for User
@@ -438,3 +438,13 @@ class GetNotifications(APIView):
 
          # return the suggestion
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, format=None):
+
+        serializer = NotificationSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
