bca619a8b630ae47e3c12d39c91881314b5eb529
TobiToxota <tobias.seiler@posteo.de>
2023-01-22 20:23:57 +0000

    removed creator on top component if current user is the creator

--- fe/src/components/features/universal/NightOutTopComponent.js
+++ fe/src/components/features/universal/NightOutTopComponent.js
@@ -20,28 +20,30 @@ const NightOutTopComponent = ({
                     Your Nightout: {nightOut.title}
                 </h3>
                 <ProgressComponent percentage={progressPercentage} />
-                <div
-                    id="creator"
-                    className="mt-2 is-inline-block is-vcentered is-flex"
-                >
-                    <button className="button is-info is-rounded p-2">
-                        <img
-                            src={`https://avatars.dicebear.com/api/${nightOut.creator.avatarStyle}/${userData.username}+${nightOut.creator.avatarIteration}.svg`}
-                            alt=""
-                            width={30}
-                        />
-                        <p className="roboto-plain"></p>
-                    </button>
-                    <p
-                        className="label is-inline-flex ml-2 pb-2 is-size-6-touch"
-                        style={{ verticalAlign: 'bottom' }}
+                {nightOut.creator.username !== userData.username ? (
+                    <div
+                        id="creator"
+                        className="mt-2 is-inline-block is-vcentered is-flex"
                     >
-                        {nightOut.creator.username !== userData.username
-                            ? nightOut.creator.username +
-                              ' has created this Nightout'
-                            : 'You created this Nightout'}
-                    </p>
-                </div>
+                        <button className="button is-info is-rounded p-2">
+                            <img
+                                src={`https://avatars.dicebear.com/api/${nightOut.creator.avatarStyle}/${userData.username}+${nightOut.creator.avatarIteration}.svg`}
+                                alt=""
+                                width={30}
+                            />
+                            <p className="roboto-plain"></p>
+                        </button>
+                        <p
+                            className="label is-inline-flex ml-2 pb-2 is-size-6-touch"
+                            style={{ verticalAlign: 'bottom' }}
+                        >
+                            {nightOut.creator.username} has created this
+                            Nightout
+                        </p>
+                    </div>
+                ) : (
+                    <></>
+                )}
             </div>
             {finalDate && (
                 <p className="label is-pulled-right is-size-5 is-size-7-touch margin-top-mobile">
