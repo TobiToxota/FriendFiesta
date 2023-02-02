bb8a86fed42c1f8a376637605cf6bba76e248934
TobiToxota <tobias.seiler@posteo.de>
2023-02-02 14:01:40 +0000

    added delete button to all entrys in FE

--- fe/src/components/features/planning/EntryComponent.js
+++ fe/src/components/features/planning/EntryComponent.js
@@ -1,14 +1,22 @@
 // local imports
-import { makeDateBeautifull } from "../../../utils/nightOutPlanningUtils"
+import { makeDateBeautifull } from '../../../utils/nightOutPlanningUtils'
 
 const EntryComponent = ({ entry, index }) => {
     return (
-            <div className="columns is-centered is-vcentered has-text-centered is-centered is-multiline mb-1">
+        <>
+            <div className="columns is-1 is-centered is-vcentered has-text-centered is-centered is-multiline mb-1">
                 <div className="column is-1 label pb-0">{index}</div>
-                <div className="column is-4 pb-0">{entry.name}</div>
+                <div className="column is-6 pb-0">{entry.name}</div>
                 <div className="column is-1 pb-0">{makeDateBeautifull(entry.startTime)}</div>
                 <div className="column is-1 pb-0">{makeDateBeautifull(entry.endTime)}</div>
+                <div className="column is-1 pb-0">
+                    <span className="icon has-text-info is-clickable">
+                        <i className="fa-regular fa-trash-can has-text-danger"></i>
+                    </span>
+                </div>
             </div>
+            <div className="container is-hidden-desktop" style={{ height: '20px' }}></div>
+        </>
     )
 }
 
--- fe/src/components/features/planning/EntrysHeaderComponent.js
+++ fe/src/components/features/planning/EntrysHeaderComponent.js
@@ -4,12 +4,19 @@ const EntrysHeaderComponent = () => {
             <label className="label mb-1 is-size-5 has-text-centered mt-4">
                 Your entrys for your suggestion:
             </label>
-                <div className="is-centered columns is-vcentered mb-0 is-centered has-text-centered is-multiline mt-1">
-                    <div className="column label is-size-4 mb-0 is-1">#</div>
-                    <div className="column label is-size-5 mb-0 is-4">Location</div>
-                    <div className="column label is-size-5 mb-0 is-1">Start</div>
-                    <div className="column label is-size-5 mb-0 is-1">End</div>
+            <div
+                className="is-centered columns is-1 is-vcentered is-centered has-text-centered is-multiline mt-1 is-hidden-mobile"
+                style={{ marginBottom: '0px' }}
+            >
+                <div className="column label is-size-4 is-1" style={{ marginBottom: '0px' }}>
+                    #
                 </div>
+                <div className="column label is-size-5 mb-0 is-6">Location</div>
+                <div className="column label is-size-5 mb-0 is-1">Start</div>
+                <div className="column label is-size-5 mb-0 is-1">End</div>
+                <div className="column label is-size-5 mb-0 is-1">Delete</div>
+            </div>
+            <div className="container is-hidden-desktop" style={{ height: '20px' }}></div>
         </>
     )
 }
