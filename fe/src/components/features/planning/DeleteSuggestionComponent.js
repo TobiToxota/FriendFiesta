e20f2dbc9b5c16e9eb53b17b790e22e454f8822a
TobiToxota <tobias.seiler@posteo.de>
2023-02-03 11:44:13 +0000

    added animation to deletesugggestion

--- fe/src/components/features/planning/DeleteSuggestionComponent.js
+++ fe/src/components/features/planning/DeleteSuggestionComponent.js
@@ -1,44 +1,23 @@
 // local imports
 import { useDeleteSuggestion } from '../../../hooks/api/suggestionAPI'
+import { shaking } from '../../../hooks/animations/animations'
 
 const DeleteSuggestionComponent = ({ suggestion, token }) => {
     // get the useDeleteSuggestionHook
-    const { deleteSuggestion, deleteSuggestionFetching } = useDeleteSuggestion(token, suggestion)
+    const { deleteSuggestion } = useDeleteSuggestion(token, suggestion)
     return (
-        <>
-        <div className="container is-hidden-mobile" style={{ height: '25px', position: 'absolute'}}>
-            {!deleteSuggestionFetching ? (
-                <button
-                    className="button is-danger is-rounded ml-1 is-small mt- is-clickable is-pulled-right "
-                    onClick={() => deleteSuggestion()}
-                    style={{ float: 'right' }}
-                >
-                    <span className="icon is-small">
-                        <i className="fa-solid fa-trash-can"></i>
-                    </span>
-                </button>
-            ) : (
-                <button className="button is-danger is-rounded ml-1 is-small is-pulled-right mt-0 is-loading">
-                    <span className="icon is-small">
-                        <i className="fa-solid fa-trash-can"></i>
-                    </span>
-                </button>
-            )}
+        <div style={{ transform: 'translateX(19px)' }}>
+            <span
+                className="icon is-small is-clickable is-pulled-right has-text-danger is-size-6-touch is-size-5"
+                id="delete-suggestion-trash"
+                onClick={() => {
+                    deleteSuggestion()
+                    shaking('#delete-suggestion-trash')
+                }}
+            >
+                <i className="fa-solid fa-trash-can"></i>
+            </span>
         </div>
-        <div className="is-hidden-tablet" style={{ height: '25px', transform: 'translateX(19px)'}}>
-            {!deleteSuggestionFetching ? (
-                    <span className="icon is-small is-clickable is-pulled-right has-text-danger" 
-                    onClick={() => deleteSuggestion()}>
-                        <i className="fa-solid fa-trash-can"></i>
-                    </span>
-            ) : (
-                <span className="icon is-small is-clickable is-pulled-right has-text-danger is-loading" 
-                    onClick={() => deleteSuggestion()}>
-                        <i className="fa-solid fa-trash-can"></i>
-                    </span>
-            )}
-        </div>
-        </>
     )
 }
 
