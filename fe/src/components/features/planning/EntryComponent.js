e56eb46e79e6f494ee891fc41fc43a3442de0393
TobiToxota <tobias.seiler@posteo.de>
2023-02-05 15:29:53 +0000

    added link to google place for google place entrys

--- fe/src/components/features/planning/EntryComponent.js
+++ fe/src/components/features/planning/EntryComponent.js
@@ -12,11 +12,10 @@ const EntryComponent = ({ entry, index, token, loadSuggestion }) => {
     const [showModal, setShowModal] = useState(false)
 
     // get the useDeleteEntryfromSuggestionhook
-    const {deleteEntryFromSuggestion} = useDeleteEntryFromSuggestion(loadSuggestion, token)
+    const { deleteEntryFromSuggestion } = useDeleteEntryFromSuggestion(loadSuggestion, token)
 
     return (
         <>
-        
             <div
                 className="entry-component columns shadow  is-1 is-centered pt-0 has-text-centered is-centered is-multiline mb-1 is-clickable"
                 style={{ backgroundColor: '#f1f1f1', borderRadius: '10px' }}
@@ -28,14 +27,23 @@ const EntryComponent = ({ entry, index, token, loadSuggestion }) => {
                 <div className="column is-1 pb-0">{makeDateBeautiful(entry.startTime)}</div>
                 <div className="column is-1 pb-0">{makeDateBeautiful(entry.endTime)}</div>
                 <div className="column is-1 pb-0">
-                    <span className="icon has-text-info is-clickable" onClick={(event) => {event.stopPropagation(); shaking('#trash'); deleteEntryFromSuggestion(entry.id)}}>
-                        <i className="fa-regular fa-trash-can has-text-danger" id='trash'></i>
+                    <span
+                        className="icon has-text-info is-clickable"
+                        onClick={(event) => {
+                            event.stopPropagation()
+                            shaking('#trash')
+                            deleteEntryFromSuggestion(entry.id)
+                        }}
+                    >
+                        <i className="fa-regular fa-trash-can has-text-danger" id="trash"></i>
                     </span>
                 </div>
             </div>
-            <StandardModalComponent showModal={showModal} setShowModal={setShowModal}
-                    children={<EntryDetailsComponent entry={entry} token={token}/> }
-                />
+            <StandardModalComponent
+                showModal={showModal}
+                setShowModal={setShowModal}
+                children={<EntryDetailsComponent entry={entry} token={token} />}
+            />
             <div className="container" style={{ height: '10px' }}></div>
             <div className="container is-hidden-tablet" style={{ height: '20px' }}></div>
         </>
--- fe/src/components/features/planning/EntryDetailsComponentl.js
+++ fe/src/components/features/planning/EntryDetailsComponentl.js
@@ -1,3 +1,4 @@
+// local imports
 import { makeDateBeautiful, splitLocationType } from '../../../utils/nightOutPlanningUtils'
 
 const EntryDetailsComponent = ({ entry, token }) => {
@@ -5,49 +6,57 @@ const EntryDetailsComponent = ({ entry, token }) => {
         <div className="columns is-vcentered">
             <div className="column is-6 ml-2">
                 <div className="container mb-2">
+                    {entry.formType === 'Google Maps' ? (
+                        <a
+                            href={'https:/www.google.de/maps/search/' + entry.name}
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <span className="is-size-4 has-text-weight-semibold">{entry.name}</span>
+                        </a>
+                    ) : (
                         <span className="is-size-4 has-text-weight-semibold">{entry.name}</span>
+                    )}
                 </div>
                 <div className="container mb-2">
-                        <span className="is-size-4 is-size-5-touch">
-                            {!entry.location ? <>Address: Not defined</> : entry.location}
-                        </span>
+                    <span className="is-size-4 is-size-5-touch">
+                        {!entry.location ? <>Address: Not defined</> : entry.location}
+                    </span>
                 </div>
                 <div className="container mb-2">
-
-                        <span className="is-size-4  is-size-5-touch">
-                            {!entry.locationType ? (
-                                <>Location-Type: Not defined</>
-                            ) : (
-                                splitLocationType(entry.locationType)
-                            )}
-                        </span>
+                    <span className="is-size-4  is-size-5-touch">
+                        {!entry.locationType ? (
+                            <>Location-Type: Not defined</>
+                        ) : (
+                            splitLocationType(entry.locationType)
+                        )}
+                    </span>
                 </div>
                 <div className="container mb-2">
-
-                        <span className="is-size-4  is-size-5-touch">
-                            From: {makeDateBeautiful(entry.startTime)} to:{' '}
-                            {makeDateBeautiful(entry.endTime)}
-                        </span>
+                    <span className="is-size-4  is-size-5-touch">
+                        From: {makeDateBeautiful(entry.startTime)} to:{' '}
+                        {makeDateBeautiful(entry.endTime)}
+                    </span>
                 </div>
                 <div className="container">
-
-                        <span className="is-size-4  is-size-5-touch">
-                            Rating: {!entry.rating ? <>No Google Maps Place</> : entry.rating}
-                        </span>
+                    <span className="is-size-4  is-size-5-touch">
+                        Rating: {!entry.rating ? <>No Google Maps Place</> : entry.rating}
+                    </span>
                 </div>
             </div>
             <div className="column has-text-centered">
                 {entry.photoKey ? (
                     <figure className="image mx-auto">
                         <img
-                            className='mx-auto mb-2'
+                            className="mx-auto mb-2"
                             src={entry.photoKey}
                             alt={entry.name}
                             style={{ borderRadius: '12px', width: '150px' }}
                         />
                         <span className="has-text-primary-dark">
-                        If you want to edit this entry, just click on the edit button in the list.
-                    </span>
+                            If you want to edit this entry, just click on the edit button in the
+                            list.
+                        </span>
                     </figure>
                 ) : (
                     <span className="my-auto has-text-primary-dark">
