5698be569943a947a73d9cd7d17347a9db542c1a
TobiToxota <tobias.seiler@posteo.de>
2023-02-05 14:22:22 +0000

    added deleteentryfromsuggestiont o FE

--- fe/src/components/features/planning/EntryComponent.js
+++ fe/src/components/features/planning/EntryComponent.js
@@ -21,13 +21,14 @@ const EntryComponent = ({ entry, index, token, loadSuggestion }) => {
                 className="entry-component columns shadow  is-1 is-centered pt-0 has-text-centered is-centered is-multiline mb-1 is-clickable"
                 style={{ backgroundColor: '#f1f1f1', borderRadius: '10px' }}
                 id="mx-mobile"
+                onClick={() => setShowModal(true)}
             >
                 <div className="column is-1 label pb-0">{index}</div>
                 <div className="column is-6 label pb-0">{entry.name}</div>
                 <div className="column is-1 pb-0">{makeDateBeautiful(entry.startTime)}</div>
                 <div className="column is-1 pb-0">{makeDateBeautiful(entry.endTime)}</div>
                 <div className="column is-1 pb-0">
-                    <span className="icon has-text-info is-clickable" onClick={() => {shaking('#trash'); deleteEntryFromSuggestion(entry.id)}}>
+                    <span className="icon has-text-info is-clickable" onClick={(event) => {event.stopPropagation(); shaking('#trash'); deleteEntryFromSuggestion(entry.id)}}>
                         <i className="fa-regular fa-trash-can has-text-danger" id='trash'></i>
                     </span>
                 </div>
