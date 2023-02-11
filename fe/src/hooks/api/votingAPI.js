f104a7933f99fc45aa323b533ecaf50f34933ab8
TobiToxota <tobias.seiler@posteo.de>
2023-02-11 14:47:26 +0000

    created useDeclareAbstention hook

--- fe/src/hooks/api/votingAPI.js
+++ fe/src/hooks/api/votingAPI.js
@@ -2,7 +2,7 @@
 import { useState } from 'react'
 import { toast } from 'react-toastify'
 
-const useCreateNewVote = ( token, refreshNightOut, getParticipantInfos, uuid ) => {
+const useCreateNewVote = (token, refreshNightOut, getParticipantInfos, uuid) => {
     const [newVoteFetching, setNewVoteFetching] = useState(false)
 
     const createNewVote = async (suggestionId) => {
@@ -33,4 +33,37 @@ const useCreateNewVote = ( token, refreshNightOut, getParticipantInfos, uuid ) =
     return { createNewVote, newVoteFetching }
 }
 
-export { useCreateNewVote }
+const useDeclareAbstention = (token, nightOut, refreshNightOut, getParticipantInfos) => {
+    const [declareAbstentionFetching, setDeclareAbstentionFetching] = useState(false)
+
+    const declareAbstention = async () => {
+        setDeclareAbstentionFetching(true)
+
+        let response = await fetch(
+            process.env.REACT_APP_API_URL + 'suggestion/suggestionVote/abstention/',
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `token ${token}`,
+                },
+                body: JSON.stringify({
+                    nightOut: nightOut.uuid,
+                }),
+            }
+        )
+        if (response.status === 201) {
+            toast.success('You declared your abstention')
+
+            refreshNightOut(nightOut.uuid)
+            getParticipantInfos(uuid)
+            setDeclareAbstentionFetching(false)
+        } else {
+            toast.error('Something went wrong')
+        }
+    }
+
+    return { declareAbstention, declareAbstentionFetching }
+}
+
+export { useCreateNewVote, useDeclareAbstention }
