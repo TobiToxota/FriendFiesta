2751479d0f35f4ea1068e052e5a294da2f618fc1
TobiToxota <tobias.seiler@posteo.de>
2023-02-17 16:36:42 +0000

    created ChatComponent

---
+++ fe/src/components/features/finished/ChatComponent.js
@@ -0,0 +1,11 @@
+const ChatComponent = () => {
+    return (
+        <div className="container">
+            <p className="mb-0 heading has-text-centered is-size-4 is-size-5-touch mt-5 label">
+                Entrys:
+            </p>
+        </div>
+    )
+}
+
+export default ChatComponent
