6623dc648943748c768513967720589142c20884
TobiToxota <tobias.seiler@posteo.de>
2023-02-18 21:49:15 +0000

    created useDeleteChat Hook

--- fe/src/hooks/api/chatAPI.js
+++ fe/src/hooks/api/chatAPI.js
@@ -39,4 +39,31 @@ const useAddChat = (refreshNightOut, token, uuid) => {
     return { addChat, addChatFetching }
 }
 
-export { useAddChat }
+const useDeleteChat = (refreshNightOut, token, uuid) => {
+    const [deleteChatFetching, setDeleteChatFetching] = useState(false)
+
+    const deleteChat = async (id) => {
+        setDeleteChatFetching(true)
+
+        let response = await fetch(process.env.REACT_APP_API_URL + 'chat/', {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `token ${token}`,
+            },
+            body: JSON.stringify({
+                id: id,
+            }),
+        })
+        if (response.status === 200) {
+            toast.success('Your Message was successfully deleted.', {
+                autoClose: 2000,
+            })
+        } else {
+            toast.error('Something went wrong')
+        }
+    }
+    return { deleteChat, deleteChatFetching }
+}
+
+export { useAddChat, useDeleteChat }
